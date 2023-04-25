bf68d360e5c94f78560685d1592e8e8a5b3f259c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-04-25 16:45:43 -0400

    chore: forms components

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	let className = '';
+	export { className as class };
+</script>
+
+<div
+	class={classNames('p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6', className)}
+>
+	<slot />
+</div>
---
+++ src/lib/components/TextField.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let type: 'text' | 'number' | 'email' | 'password' | 'date' = 'text';
+	export let name: string;
+	export let id = '';
+	export let error: string | null = null;
+	export let value: string | number | Date | null = null;
+	export let label: string | null = null;
+	export let placeholder: string | undefined = undefined;
+	export let required = false;
+	export let autocomplete: 'true' | 'false' | undefined = undefined;
+	let customClassNames = '';
+	export { customClassNames as class };
+	let className = classNames(
+		customClassNames,
+		'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5'
+	);
+	const handleInput = (event: Event): void => {
+		const target = event.target as HTMLInputElement;
+		value = type.match(/^(number|range)$/) ? +target.value : target.value;
+	};
+</script>
+
+{#if label}
+	<label for={name} class="block mb-2 text-sm font-medium text-gray-900">{label}</label>
+{/if}
+
+<input
+	{name}
+	{id}
+	{placeholder}
+	{required}
+	{autocomplete}
+	{type}
+	{value}
+	class:border-red-600={!!error}
+	class={className}
+	on:change
+	on:blur
+	on:input={handleInput}
+/>
+<p class:opacity-0={!error} class="text-sm pb-1 text-red-600">{error}</p>
