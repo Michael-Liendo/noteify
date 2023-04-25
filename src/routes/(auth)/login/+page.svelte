046e897a049e29096a102f65d81c83b024c39861
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-04-25 17:31:30 -0400

    design: login

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import TextField from '$lib/components/TextField.svelte';
+</script>
+
+<svelte:head>
+	<title>Login - Noteify</title>
+</svelte:head>
+
+<div class="flex items-center justify-center min-h-screen">
+	<Card class="container md:w-[45vw] 2xl:w-[25vw]">
+		<h1 class="text-2xl font-bold text-gray-900">Welcome back!</h1>
+		<span>Don't have an account? <a href="/signup">Sign up</a></span>
+		<form class="mt-4">
+			<TextField type="email" name="email" label="Email" placeholder="Enter your email" required />
+			<TextField
+				name="password"
+				type="password"
+				label="Password"
+				placeholder="* * * * * * * * *"
+				required
+			/>
+			<Button type="submit" fullWidth>Sign in to your account</Button>
+		</form>
+	</Card>
+</div>
---
+++ src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import TextField from '$lib/components/TextField.svelte';
+</script>
+
+<svelte:head>
+	<title>Sign up - Noteify</title>
+</svelte:head>
+
+<div class="flex items-center justify-center min-h-screen">
+	<Card class="container md:w-[45vw] 2xl:w-[25vw]">
+		<h1 class="text-2xl font-bold text-gray-900">Create a account!</h1>
+		<span>You have a account? <a href="/login">login</a></span>
+		<form class="mt-4">
+			<TextField type="text" name="full_name" label="Full name" placeholder="Joe Doe" required />
+			<TextField type="email" name="email" label="Email" placeholder="Enter your email" required />
+			<TextField
+				name="password"
+				type="password"
+				label="Password"
+				placeholder="* * * * * * * * *"
+				required
+			/>
+			<Button type="submit" fullWidth>Create your account</Button>
+		</form>
+	</Card>
+</div>
