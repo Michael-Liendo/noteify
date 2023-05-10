1df40c65c7c4d290cbcaf7014de01fa0fdd38ba9
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-10 16:25:08 -0400

    design: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			...defaultTheme.colors,
+			accent: '#A9EF62',
+			primary: '#F8F8F8',
+			background: '#2D3748',
+			'background-alt': '#4A5568',
+			'button-primary': '#446dff'
+		},
 		extend: {
 			fontFamily: {
 				sans: ['Inter', ...defaultTheme.fontFamily.sans]
